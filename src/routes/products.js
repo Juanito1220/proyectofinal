d31f7a480e81f7ae468d53e225ad585e592b07fa
Juan Pablo Rodriguez <jprodriguez@dcathenea.local>
2022-11-23 12:07:56 -0500

    Prueba deploy

---
+++ src/routes/products.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+const productsControllers = require('../Controllers/productsControllers');
+const sinLogin=require("../middlewares/sinLogin");
+const accesos = require("../middlewares/accesos");
+const validator = require("../middlewares/validation");
+const multer = require("../middlewares/multerProducts");
+
+//Carrito de ptoductos
+router.get('/productCart',sinLogin, productsControllers.productCart);
+
+// Detalle producto
+router.get('/detail/:id',sinLogin, productsControllers.productDetail);
+
+//Lista de productos
+router.get('/',sinLogin, productsControllers.products);
+
+
+//Creación de productos
+router.get('/create',sinLogin,accesos, productsControllers.formCreate);
+router.post('/',multer(),validator.registerproduct, productsControllers.lista); 
+
+
+//Editar un producto
+
+router.get('/edit/:id',sinLogin, productsControllers.edit); 
+router.patch('/edit/:id',multer(),validator.registerproduct, productsControllers.update);
+
+//Borrar un producto
+
+router.delete('/delete/:id', productsControllers.eliminar); 
+
+
+
+module.exports = router;
